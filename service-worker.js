f6b4797107e9c2823692f95db6ad4c4678062f98
carlosllano75-maker <carlosllano75@gmail.com>
2026-03-02 17:09:42 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,16 @@
-const CACHE_NAME = 'casa-gourmet-v4';
-const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  './logo-casa-gourmet.jpg'
-];
+const CACHE_NAME = 'casa-gourmet-v5';
+const assets = ['./', './index.html', './manifest.json', './logo-casa-gourmet.jpg'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
-  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+  self.skipWaiting(); // <--- ESTO DESPIERTA LOS BOTONES
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim(); // <--- ESTO TOMA CONTROL INMEDIATO DE LA APP
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
